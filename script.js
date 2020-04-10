b2310ba2f8e9e528e099de045a281ec8ac5b0f36
Criscubed <criscubed@gmail.com>
2020-04-10 07:36:36 -0500

    Forismatic API is working

---
+++ script.js
@@ -0,0 +1,11 @@
+//app generates random quotes
+
+//forismatic api gets a random quote and it's corresponsding author in jsonp
+$(document).ready(function () {
+  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?").done(update);
+
+});
+
+function update(data) {
+  console.log(data.quoteText);
+};
