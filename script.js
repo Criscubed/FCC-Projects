82857e84423c14b5d312da25f2b70034d5545a23
Criscubed <criscubed@gmail.com>
2020-04-13 05:58:56 -0500

    Removed redundant div containers

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?").done(update);
   
   //add an onclick event to a button to get a new quote
-  $("#refreshButton").click(function(){
+  $("#new-quote").click(function(){
     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?").done(update);
   });
 
