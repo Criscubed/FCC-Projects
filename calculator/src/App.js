78dd8f496e45db89f945d69e35ad6fbd2d40c20c
Criscubed <criscubed@gmail.com>
2020-05-19 05:45:36 -0500

    input is rendered to display

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,9 +4,102 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    lmao
+      <Calculator />
     </div>
   );
 }
 
 export default App;
+
+class Calculator extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      display: "0"
+    };
+    this.clearDisplay = this.clearDisplay.bind(this);
+    this.inputDisplay = this.inputDisplay.bind(this);
+  }
+
+  clearDisplay() {
+    this.setState({
+      display: "0"
+    });
+  }
+
+  inputDisplay(e){
+    let temp;
+    if(this.state.display != "0"){
+      //concatonate the current button press into the input
+      temp = this.state.display.concat(e.target.textContent);
+    } else {
+      //begin constructing the input
+      temp =e.target.textContent
+    }
+    this.setState({
+      display: temp
+    });
+  }
+
+  render(){
+    return(
+      <div id="calculator">
+        <Display display={this.state.display}/>
+        <br></br>
+        <Equals />
+        <Number digit="0" number="zero" onClick={this.inputDisplay} />
+        <Number digit="1" number="one" onClick={this.inputDisplay} />
+        <Number digit="2" number="two" onClick={this.inputDisplay} />
+        <Number digit="3" number="three" onClick={this.inputDisplay} />
+        <Number digit="4" number="four" onClick={this.inputDisplay} />
+        <Number digit="5" number="five" onClick={this.inputDisplay} />
+        <Number digit="6" number="six" onClick={this.inputDisplay} />
+        <Number digit="7" number="seven" onClick={this.inputDisplay} />
+        <Number digit="8" number="eight" onClick={this.inputDisplay} />
+        <Number digit="9" number="nine" onClick={this.inputDisplay} />
+        <Operation name="add" symbol="+" />
+        <Operation name="subtract" symbol="-" />
+        <Operation name="multiply" symbol="*" />
+        <Operation name="divide" symbol="/" />
+        <Decimal name="decimal" symbol="." />
+        <Clear name="clear" symbol="c" onClick={this.clearDisplay}/>
+      </div>
+    );
+  }
+}
+
+const Display = (props) => {
+  return(
+    <div id="display">{props.display}</div>
+  );
+}
+
+const Equals = () => {
+  return(
+    <button id="equals">=</button>
+  );
+}
+
+const Number = (props) => {
+  return(
+    <button id={props.number} onClick={props.onClick}>{props.digit}</button>
+  )
+}
+
+const Operation = (props) => {
+  return(
+    <button id={props.name}>{props.symbol}</button>
+  );
+}
+
+const Decimal = (props) => {
+  return(
+    <button id={props.name}>{props.symbol}</button>
+  );
+}
+
+const Clear = (props) => {
+  return(
+    <button id={props.name} onClick={props.onClick}>{props.symbol}</button>
+  );
+}
